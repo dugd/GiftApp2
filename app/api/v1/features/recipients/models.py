476b1072a4fff463ef174cce58ea2cc5b9b44980
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-04-28 22:15:48 +0300

    create recipient model

---
+++ app/api/v1/features/recipients/models.py
@@ -0,0 +1,20 @@
+from datetime import date
+from typing import Optional, List
+
+from sqlalchemy import Integer, String, Date, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.ext.mutable import MutableDict
+from sqlalchemy.dialects.postgresql import JSONB
+from app.models.base import Base
+
+
+class Recipient(Base):
+    __tablename__ = "recipients"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
+    name: Mapped[str] = mapped_column(String(32), nullable=False)
+    birthday: Mapped[date] = mapped_column(Date, nullable=True)
+    relation: Mapped[str] = mapped_column(String(32), nullable=True)
+    preferences: Mapped[Optional[List[str]]] = mapped_column(MutableDict.as_mutable(JSONB), nullable=True)
+    notes: Mapped[Optional[str]] = mapped_column(String, nullable=True)
--- app/api/v1/features/recipients/schemas.py
+++ app/api/v1/features/recipients/schemas.py
@@ -8,7 +8,7 @@ class RecipientBase(BaseModel):
     name: str = Field(min_length=2, max_length=32)
     birthday: date
     relation: str = Field(min_length=2, max_length=32)
-    preferences: Optional[List[str]] = Field(default=None)
+    preferences: Optional[List[str]] = Field(default=None, min_length=1, max_length=10)
     notes: Optional[str] = None
 
     @classmethod
@@ -17,8 +17,9 @@ class RecipientBase(BaseModel):
         today = date.today()
         if v >= today:
             raise ValueError("Birthday must be in the past")
-        if v < date(1900, 1, 1):
-            raise ValueError("Birthday is too far in the past (more than 1900)")
+        max_past = date(1900, 1, 1)
+        if v < max_past:
+            raise ValueError(f"Birthday is too far in the past (more than {max_past.year})")
         return v
 
 
@@ -35,7 +36,7 @@ class RecipientRead(RecipientBase):
 class RecipientUpdateInfo(BaseModel):
     name: Optional[str] = Field(default=None, min_length=2, max_length=32)
     relation: Optional[str] = Field(default=None, min_length=2, max_length=32)
-    preferences: Optional[List[str]] = Field(default=None)
+    preferences: Optional[List[str]] = Field(default=None, min_length=1, max_length=10)
     notes: Optional[str] = None
 
 
@@ -48,6 +49,7 @@ class RecipientUpdateBirthday(BaseModel):
         today = date.today()
         if v >= today:
             raise ValueError("Birthday must be in the past")
-        if v < date(1900, 1, 1):
-            raise ValueError("Birthday is too far in the past (more than 1900)")
+        max_past = date(1900, 1, 1)
+        if v < max_past:
+            raise ValueError(f"Birthday is too far in the past (more than {max_past.year})")
         return v
