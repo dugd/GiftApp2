2c83de909d60ba6eaef1f90817df0c8b0089506e
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-04-29 12:23:30 +0300

    implement TokenBearer dependency

--- app/api/v1/features/auth/dependencies.py
+++ app/api/v1/features/auth/dependencies.py
@@ -1,5 +1,6 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordBearer, HTTPBearer
+from jose import JWTError, ExpiredSignatureError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
@@ -7,17 +8,57 @@ from app.api.v1.features.auth.models import User
 from app.api.v1.features.auth.security import decode_token
 from app.core.database import get_session
 
+
+class TokenBearer(HTTPBearer):
+    def __init__(self, auto_error=True):
+        super().__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> dict:
+        creds = await super().__call__(request)
+
+        token = creds.credentials
+
+        try:
+            token_data = decode_token(token)
+        except ExpiredSignatureError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
+        except JWTError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+        self.verify_token_data(token_data)
+
+        return token_data
+
+
+    def verify_token_data(self, token_data):
+        raise NotImplementedError("Override this method in child classes")
+
+
+class AccessTokenBearer(TokenBearer):
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and token_data.get("type") != "access":
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+
+class RefreshTokenBearer(TokenBearer):
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and token_data.get("type") != "refresh":
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
-bearer_scheme = HTTPBearer()
+access_token_scheme = AccessTokenBearer()
+refresh_token_scheme = RefreshTokenBearer()
 
 
-async def get_current_user(db: AsyncSession = Depends(get_session), token: str = Depends(oauth2_scheme)) -> User:
-    payload = decode_token(token)
-    if not payload or "sub" not in payload:
+async def get_current_user(
+        db: AsyncSession = Depends(get_session),
+        token_payload: dict = Depends(access_token_scheme)) -> User:
+    if "id" not in token_payload:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
     async with db.begin():
-        query = select(User).where(User.email == payload["sub"])
+        query = select(User).where(User.id == token_payload["id"])
         result = await db.execute(query)
         user = result.scalar_one_or_none()
 
@@ -25,3 +66,13 @@ async def get_current_user(db: AsyncSession = Depends(get_session), token: str =
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
 
     return user
+
+
+class RoleChecker:
+    def __init__(self, allowed_roles: list[str]):
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, user: User = Depends(get_current_user)) -> bool:
+        if not user.role in self.allowed_roles:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden")
+        return True
--- app/api/v1/features/auth/security.py
+++ app/api/v1/features/auth/security.py
@@ -10,20 +10,24 @@ def hash_password(password: str) -> str:
     hashed = bcrypt.hashpw(password_bytes, salt)
     return hashed.decode("UTF-8")
 
+
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(plain_password.encode("UTF-8"), hashed_password.encode("UTF-8"))
 
-def create_access_token(payload: dict, expires_delta: timedelta = timedelta(minutes=15)):
+
+def create_jwt_token(payload: dict, expires_delta: timedelta) -> str:
     to_encode = payload.copy()
     expire = datetime.utcnow() + expires_delta
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
 
+def create_access_token(payload: dict, expires_delta: timedelta = timedelta(minutes=15)):
+    return create_jwt_token(payload, expires_delta)
+
+
 def create_refresh_token(payload: dict, expires_delta: timedelta = timedelta(days=30)):
-    to_encode = payload.copy()
-    expire = datetime.utcnow() + expires_delta
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+    return create_jwt_token(payload, expires_delta)
+
 
 def decode_token(token: str):
     payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
