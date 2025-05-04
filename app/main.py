a506d5bba25df0cb83a4227bb428a16bae0b77c7
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-05-04 14:20:36 +0300

    add APScheduler for occur generation every day

--- app/main.py
+++ app/main.py
@@ -1,9 +1,28 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from apscheduler.triggers.cron import CronTrigger
 
 from app.core.config import settings
+from app.sheduler import scheduler, run_generate_occur
 from app.api.v1.router import api_router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    print("Starting APScheduler...")
+    scheduler.add_job(
+        run_generate_occur,
+        CronTrigger(hour=0, minute=0),
+        id='generate_missing_occurrences',
+        replace_existing=True,
+    )
+    scheduler.start()
+    yield
+    print("Shutdown APScheduler...")
+    scheduler.shutdown(wait=False)
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(api_router)
 
 @app.get("/")
