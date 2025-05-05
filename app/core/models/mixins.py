4773895ff919716c3eb4c9376a2af03e9b32342c
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-05-05 11:19:53 +0300

    add GUID type and SurrogatePKMixin

--- app/core/models/mixins.py
+++ app/core/models/mixins.py
@@ -1,8 +1,46 @@
+from uuid import UUID, uuid4
 from datetime import datetime
 
-from sqlalchemy import func
+from sqlalchemy import func, Dialect
 from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy.types import TIMESTAMP
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+from sqlalchemy.types import TIMESTAMP, TypeDecorator, CHAR
+
+
+class GUID(TypeDecorator):
+    impl = CHAR
+
+    def load_dialect_impl(self, dialect: Dialect):
+        if dialect.name == "postgresql":
+            return dialect.type_descriptor(PG_UUID(as_uuid=True))
+        else:
+            return dialect.type_descriptor(CHAR(32))
+
+    def process_bind_param(self, value, dialect: Dialect):
+        if value is None or dialect.name == "postgresql":
+            return value
+        else:
+            if not isinstance(value, UUID):
+                value = UUID(value)
+            return value.hex
+
+    def process_result_value(
+        self, value, dialect: Dialect
+    ):
+        if value is None:
+            return value
+        else:
+            if not isinstance(value, UUID):
+                value = UUID(value)
+            return value
+
+
+class SurrogatePKMixin:
+    id: Mapped[UUID] = mapped_column(
+        GUID(),
+        primary_key=True,
+        default=uuid4,
+    )
 
 
 class TimestampMixin:
